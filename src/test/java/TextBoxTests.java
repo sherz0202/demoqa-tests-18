72696f0375432edeb32abcf981cdab8057aa801a
raxmatov <sherz0202@bk.ru>
2024-11-28 10:21:42 +0500

    second

---
+++ src/test/java/TextBoxTests.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void BeforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void FirstTest(){
+        open("/text-box");
+
+        $("#userName").setValue("Sherzod");
+        $("#userEmail").setValue("sherz0202@bk.ru");
+        $("#currentAddress").setValue("Aviasozlar");
+        $("#permanentAddress").setValue("1 11 1");
+        $("#submit").click();
+
+
+        $("#output").shouldHave(text("Sherzod"), text("sherz0202@bk.ru"), text("Aviasozlar"), text("1 11 1"));
+
+
+
+
+    }
+}
